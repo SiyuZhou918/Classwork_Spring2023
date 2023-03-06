d5c59e3af7784c924364b29f66a0216d6905ae4e
SiyuZhou <syzhou918@gmail.com>
2023-03-06 11:50:22 -0500

    0

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -13,14 +13,27 @@ def info_route():
 
 @app.route("/HDL_analysis", methods=["POST"])
 def HDL_route_handler():
-    ''''''
-    in_data = {"name" : <patient_name>,
-               "HDL_value": <HDL_results> }
-    ''''''
+    """"""
+    in_data = {"name" : patient_name, 
+               "HDL_value": <HDL_results>}
+    """"""
     from blood_calculator import HDL_analysis
     in_data = request.get_json()
     diagnosis = HDL_analysis(in_data[HDL_value])
     return diagnosis
+
+@app.route("/add", methods=["POST"])
+def add_numbers():
+    in_data = request.get_json()
+    answer = in_data["a"] + in_data["b"]
+    if answer < 0:
+        return "The answer was less than zero. BAD", 400
+    return jsonify(answer)
+
+@app.route("/add_two/<a>/<b>", methods=["GET"])
+def add_two_handlere(a, b):
+    answer == a + b
+    return jsonify(answer)
 
 if __name__ == "__main__":
     app.run()

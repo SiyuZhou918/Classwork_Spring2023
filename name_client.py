e2539cf1caaaa8866d70e8318fbb2b9d438d03fe
SiyuZhou <syzhou918@gmail.com>
2023-03-15 15:29:38 -0400

    in_class_exercise_blood_matching_Mar_1

--- name_client.py
+++ name_client.py
@@ -1,7 +1,5 @@
 import requests
 
-# server = "https://vcm-21179.vm.duke"
-
 out_data = {"name": "Siyu Zhou", "net_id": "sz274",
             "e-mail": "siyu.zhou@duke.edu"}
 r = requests.post("http://vcm-21170.vm.duke.edu:5000/student", json=out_data)
@@ -10,3 +8,7 @@ print(r.text)
 
 r = requests.get("http://vcm-21170.vm.duke.edu:5000/list")
 print(r.text)
+
+
+
+
